b15d5a0e0987ccc3475d70bf84727947173a8308
Cristian Jurado <ernestojurado2004@gmail.com>
2025-06-04 01:26:53 -0500

    Se solucionó un conflicto en el archivo Conexion.php

--- core/Conexion.php
+++ core/Conexion.php
@@ -1,27 +1,4 @@
 <?php
-<<<<<<< HEAD
-class Database {
-    private static $host = "localhost:3306";
-    private static $db_name = "evento";
-    private static $username = "root";
-    private static $password = "";
-    private static $conn;
-
-    public static function getConnection() {
-        if (self::$conn == null) {
-            try {
-                self::$conn = new PDO(
-                    "mysql:host=" . self::$host . ";dbname=" . self::$db_name . ";charset=utf8mb4",
-                    self::$username,
-                    self::$password
-                );
-                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch(PDOException $exception) {
-                die("Error de conexión: " . $exception->getMessage());
-            }
-        }
-        return self::$conn;
-=======
 require_once '../config/config.php';
 
 class Conexion {
@@ -42,7 +19,7 @@ class Conexion {
             // Mensaje útil durante desarrollo (puedes ocultarlo en producción)
             die("❌ Error al conectar a la base de datos: " . $e->getMessage());
         }
->>>>>>> feature/Modulo-Eventos-Sesiones
+
     }
 }
 ?>
