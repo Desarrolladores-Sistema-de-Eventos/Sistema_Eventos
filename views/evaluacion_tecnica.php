f1ce715d6af37a2efd26297186b5db6c649fd1d7
Javier <dquisaguano6335@uta.edu.ec>
2025-06-04 23:41:11 -0500

    feat: crear solicitud_usuario.php con frontend completo y conexión lista al backend
    - Se creó el archivo solicitud_usuario.php con toda la estructura y validación del frontend
    - Se preparó la lógica para conexión con el backend
    - Se actualizaron IDs y names en el formulario de evaluación técnica para alinearlos con la base de datos

--- views/evaluacion_tecnica.php
+++ views/evaluacion_tecnica.php
@@ -1,6 +1,13 @@
 <?php include("partials/header_Admin.php"); ?>
 <?php include("../core/auth.php"); ?>
 
+<style>
+  .is-invalid {
+    border: 1px solid red !important;
+    background-color: #ffe6e6;
+  }
+</style>
+
 <div id="page-wrapper">
   <div id="page-inner">
     <div class="row">
@@ -13,7 +20,6 @@
     <div class="panel panel-default">
       <div class="panel-heading"><i class="fa fa-list"></i> Lista de Solicitudes</div>
       <div class="panel-body">
-
         <div class="table-responsive">
           <table class="table table-bordered table-striped" id="tabla-solicitudes">
             <thead class="thead-dark">
@@ -30,23 +36,22 @@
               <tr>
                 <td>1</td>
                 <td>Ana Ruiz</td>
-                <td>CAM-2024-001</td>
+                <td>1</td>
                 <td>2025-06-01</td>
                 <td>Pendiente</td>
                 <td>
                   <button type="button" class="btn btn-info btn-ver"
                     data-toggle="tooltip" title="Ver solicitud"
                     data-nombre="Ana Ruiz"
-                    data-codigo="CAM-2024-001"
+                    data-codigo="1"
                     data-fecha="2025-06-01"
                     data-modulo="Matrícula">
                     <i class="fa fa-eye"></i>
                   </button>
-
                   <button type="button" class="btn btn-success btn-evaluar"
                     data-toggle="tooltip" title="Evaluar cambio"
                     data-nombre="Ana Ruiz"
-                    data-codigo="CAM-2024-001"
+                    data-codigo="1"
                     data-fecha="2025-06-01"
                     data-modulo="Matrícula">
                     <i class="fa fa-pencil"></i>
@@ -61,7 +66,6 @@
         <div class="modal fade" id="modalEvaluacion" tabindex="-1" role="dialog" aria-labelledby="modalEvaluacionLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
-
               <div class="modal-header">
                 <h5 class="modal-title" id="modalEvaluacionLabel"><i class="fa fa-edit"></i> Evaluación Técnica del Cambio</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
@@ -70,27 +74,27 @@
               </div>
 
               <div class="modal-body">
-                <form id="formEvaluacion">
+                <form id="formEvaluacion" method="POST" action="guardar_evaluacion.php">
                   <div class="panel panel-info">
-                    <div class="panel-heading"><i class="fa fa-info-circle"></i> Información general</div>
+                    <div class="panel-heading">Información general</div>
                     <div class="panel-body">
                       <div class="row">
                         <div class="col-md-6">
-                          <label><i class="fa fa-barcode"></i> Código del cambio</label>
-                          <input type="text" class="form-control" id="codigo" name="codigo" readonly>
+                          <label>Código del cambio</label>
+                          <input type="text" class="form-control" id="SECUENCIAL_CAMBIO" name="SECUENCIAL_CAMBIO" readonly>
                         </div>
                         <div class="col-md-6">
-                          <label><i class="fa fa-user"></i> Nombre del solicitante</label>
+                          <label>Nombre del solicitante</label>
                           <input type="text" class="form-control" id="solicitante" name="solicitante" readonly>
                         </div>
                       </div><br>
                       <div class="row">
                         <div class="col-md-6">
-                          <label><i class="fa fa-calendar"></i> Fecha de solicitud</label>
+                          <label>Fecha de solicitud</label>
                           <input type="date" class="form-control" id="fecha_solicitud" name="fecha_solicitud" readonly>
                         </div>
                         <div class="col-md-6">
-                          <label><i class="fa fa-cubes"></i> Módulo afectado</label>
+                          <label>Módulo afectado</label>
                           <input type="text" class="form-control" id="modulo" name="modulo" readonly>
                         </div>
                       </div>
@@ -98,25 +102,26 @@
                   </div>
 
                   <div class="panel panel-warning">
-                    <div class="panel-heading"><i class="fa fa-sliders"></i> Evaluación técnica</div>
+                    <div class="panel-heading">Evaluación técnica</div>
                     <div class="panel-body">
                       <div class="form-group">
-                        <label><i class="fa fa-tasks"></i> Tipo de cambio (ITIL)</label><br>
-                        <label class="radio-inline"><input type="radio" name="tipo_itil" value="Estándar"> Estándar</label>
-                        <label class="radio-inline"><input type="radio" name="tipo_itil" value="Normal"> Normal</label>
-                        <label class="radio-inline"><input type="radio" name="tipo_itil" value="Emergencia"> Emergencia</label>
+                        <label>Tipo de cambio (ITIL)</label><br>
+                        <label class="radio-inline"><input type="radio" name="TIPO_ITIL" value="Estándar"> Estándar</label>
+                        <label class="radio-inline"><input type="radio" name="TIPO_ITIL" value="Normal"> Normal</label>
+                        <label class="radio-inline"><input type="radio" name="TIPO_ITIL" value="Emergencia"> Emergencia</label>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-exclamation-triangle"></i> Prioridad</label><br>
-                        <label class="radio-inline"><input type="radio" name="prioridad" value="Alta"> Alta</label>
-                        <label class="radio-inline"><input type="radio" name="prioridad" value="Media"> Media</label>
-                        <label class="radio-inline"><input type="radio" name="prioridad" value="Baja"> Baja</label>
+                        <label>Prioridad</label><br>
+                        <label class="radio-inline"><input type="radio" name="PRIORIDAD" value="Alta"> Alta</label>
+                        <label class="radio-inline"><input type="radio" name="PRIORIDAD" value="Media"> Media</label>
+                        <label class="radio-inline"><input type="radio" name="PRIORIDAD" value="Baja"> Baja</label>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-wrench"></i> Categoría técnica</label>
-                        <select class="form-control" id="categoria" name="categoria">
+                        <label>Categoría técnica</label>
+                        <select class="form-control" name="CATEGORIA_TECNICA">
+                          <option value="">-- Seleccione --</option>
                           <option>Frontend / Interfaz</option>
                           <option>Backend / Lógica del sistema</option>
                           <option>Base de datos</option>
@@ -127,50 +132,50 @@
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-lightbulb-o"></i> Evaluación técnica / impacto</label>
-                        <textarea class="form-control" id="evaluacion" name="evaluacion" rows="2"></textarea>
+                        <label>Evaluación técnica / impacto</label>
+                        <textarea class="form-control" name="EVALUACION" rows="2"></textarea>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-thumbs-up"></i> Beneficios del cambio</label>
-                        <textarea class="form-control" id="beneficios" name="beneficios" rows="2"></textarea>
+                        <label>Beneficios del cambio</label>
+                        <textarea class="form-control" name="BENEFICIOS" rows="2"></textarea>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-thumbs-down"></i> Impacto de no realizar el cambio</label>
-                        <textarea class="form-control" id="impacto_negativo" name="impacto_negativo" rows="2"></textarea>
+                        <label>Impacto de no realizar el cambio</label>
+                        <textarea class="form-control" name="IMPACTO_NEGATIVO" rows="2"></textarea>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-list-ul"></i> Acciones requeridas</label>
-                        <textarea class="form-control" id="acciones" name="acciones" rows="2"></textarea>
+                        <label>Acciones requeridas</label>
+                        <textarea class="form-control" name="ACCIONES" rows="2"></textarea>
                       </div>
                     </div>
                   </div>
 
                   <div class="panel panel-success">
-                    <div class="panel-heading"><i class="fa fa-check-circle"></i> Decisión y seguimiento</div>
+                    <div class="panel-heading">Decisión y seguimiento</div>
                     <div class="panel-body">
                       <div class="form-group">
-                        <label><i class="fa fa-balance-scale"></i> Decisión</label><br>
-                        <label class="radio-inline"><input type="radio" name="estado" value="Aprobado"> Aprobado</label>
-                        <label class="radio-inline"><input type="radio" name="estado" value="Rechazado"> Rechazado</label>
-                        <label class="radio-inline"><input type="radio" name="estado" value="Más información"> Solicita más info</label>
+                        <label>Decisión</label><br>
+                        <label class="radio-inline"><input type="radio" name="DECISION" value="Aprobado"> Aprobado</label>
+                        <label class="radio-inline"><input type="radio" name="DECISION" value="Rechazado"> Rechazado</label>
+                        <label class="radio-inline"><input type="radio" name="DECISION" value="Más información"> Solicita más info</label>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-comments"></i> Observaciones</label>
-                        <textarea class="form-control" id="observaciones" name="observaciones" rows="2"></textarea>
+                        <label>Observaciones</label>
+                        <textarea class="form-control" name="OBSERVACIONES" rows="2"></textarea>
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-calendar-check-o"></i> Fecha de decisión</label>
-                        <input type="date" class="form-control" id="fecha_decision" name="fecha_decision">
+                        <label>Fecha de decisión</label>
+                        <input type="date" class="form-control" name="FECHA_DECISION">
                       </div>
 
                       <div class="form-group">
-                        <label><i class="fa fa-user-circle"></i> Responsable técnico</label>
-                        <input type="text" class="form-control" id="responsable" name="responsable">
+                        <label>Responsable técnico</label>
+                        <input type="text" class="form-control" name="RESPONSABLE_TECNICO">
                       </div>
                     </div>
                   </div>
@@ -221,19 +226,83 @@ $(document).ready(function () {
 
     $('#formEvaluacion')[0].reset();
     $('#solicitante').val(nombre);
-    $('#codigo').val(codigo);
+    $('#SECUENCIAL_CAMBIO').val(codigo);
     $('#fecha_solicitud').val(fecha);
     $('#modulo').val(modulo);
 
-    setTimeout(() => {
-      $('#modalEvaluacion').modal('show');
-    }, 100);
+    $('#formEvaluacion .form-control, #formEvaluacion textarea').removeClass('is-invalid');
+    $('#modalEvaluacion').modal('show');
   });
 
   $('#formEvaluacion').submit(function (e) {
     e.preventDefault();
-    Swal.fire('Guardado', 'Evaluación registrada (simulada).', 'success');
-    $('#modalEvaluacion').modal('hide');
+
+    let errores = [];
+
+    $('#formEvaluacion .form-control, #formEvaluacion textarea').removeClass('is-invalid');
+
+    function marcarInvalido(selector) {
+      $(selector).addClass('is-invalid');
+    }
+
+    const tipo = $('input[name="TIPO_ITIL"]:checked').val();
+    const prioridad = $('input[name="PRIORIDAD"]:checked').val();
+    const categoria = $('select[name="CATEGORIA_TECNICA"]').val();
+    const evaluacion = $('textarea[name="EVALUACION"]');
+    const beneficios = $('textarea[name="BENEFICIOS"]');
+    const impacto = $('textarea[name="IMPACTO_NEGATIVO"]');
+    const acciones = $('textarea[name="ACCIONES"]');
+    const decision = $('input[name="DECISION"]:checked').val();
+    const fecha_decision = $('input[name="FECHA_DECISION"]');
+    const responsable = $('input[name="RESPONSABLE_TECNICO"]');
+
+    if (!tipo) errores.push("Selecciona un tipo de cambio.");
+    if (!prioridad) errores.push("Selecciona la prioridad.");
+    if (!categoria) {
+      errores.push("Selecciona la categoría técnica.");
+      marcarInvalido('select[name="CATEGORIA_TECNICA"]');
+    }
+    if (evaluacion.val().trim().length < 10) {
+      errores.push("Evaluación técnica debe tener mínimo 10 caracteres.");
+      marcarInvalido(evaluacion);
+    }
+    if (beneficios.val().trim().length < 10) {
+      errores.push("Beneficios deben tener mínimo 10 caracteres.");
+      marcarInvalido(beneficios);
+    }
+    if (impacto.val().trim().length < 10) {
+      errores.push("Impacto negativo debe tener mínimo 10 caracteres.");
+      marcarInvalido(impacto);
+    }
+    if (acciones.val().trim().length < 10) {
+      errores.push("Acciones requeridas deben tener mínimo 10 caracteres.");
+      marcarInvalido(acciones);
+    }
+    if (!decision) errores.push("Selecciona una decisión.");
+    if (responsable.val().trim() === "") {
+      errores.push("Responsable técnico es obligatorio.");
+      marcarInvalido(responsable);
+    }
+
+    const hoy = new Date().toISOString().split("T")[0];
+    if (!fecha_decision.val()) {
+      errores.push("La fecha de decisión es obligatoria.");
+      marcarInvalido(fecha_decision);
+    } else if (fecha_decision.val() > hoy) {
+      errores.push("La fecha de decisión no puede estar en el futuro.");
+      marcarInvalido(fecha_decision);
+    }
+
+    if (errores.length > 0) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Faltan campos o contienen errores',
+        html: '<ul style="text-align:left;">' + errores.map(e => `<li>${e}</li>`).join('') + '</ul>'
+      });
+      return;
+    }
+
+    this.submit();
   });
 });
 </script>
